674b0f16f77934cc7beba4db0f8ce47e9510999c
IlanG96 <Ilangold123>
2021-10-27 20:36:53 +0300

    First Version+few Tests

--- src/ex0/Ex0_main.java
+++ src/ex0/Ex0_main.java
@@ -10,7 +10,7 @@ import ex0.simulator.Simulator_A;
  * starting to implement the algorithm.
  */
 public class Ex0_main {
-    public static Long ID0=123456789L, ID1 = null, ID2 = null;
+    public static Long ID0=315804583L, ID1 = null, ID2 = null;
     public static void main(String[] ar) {
         String codeOwner = codeOwner();
         Simulator_A.setCodeOwner(codeOwner);
--- src/ex0/algo/RealElevatorAlgoTest.java
+++ src/ex0/algo/RealElevatorAlgoTest.java
@@ -1,11 +1,66 @@
 package ex0.algo;
 
+import ex0.Building;
+import ex0.CallForElevator;
+import ex0.Elevator;
+import ex0.simulator.Call_A;
+import ex0.simulator.ElevetorCallList;
+import ex0.simulator.Simulator_A;
 import org.junit.jupiter.api.Test;
 
-public class RealElevatorAlgoTest {
-    @Test
-    void getBuilding(){
-        assert true;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RealElevatorAlgoTest {
+
+
+   private Building B1;
+    private Building B9;
+    private  RealElevatorAlgo MyAlgo1;
+    private  RealElevatorAlgo MyAlgo9;
+    private ElevetorCallList c;
+    private Call_A call;
+    private Elevator[] ElevatorList;
+
+    public RealElevatorAlgoTest(){
+
+        Simulator_A.initData(1,null);
+        B1 = Simulator_A.getBuilding();
+        Simulator_A.initData(9,null);
+        B9 = Simulator_A.getBuilding();
+        c=new ElevetorCallList();
+        call=new Call_A(1,2,3);
+        c.add(call);
+        MyAlgo1 = new RealElevatorAlgo(B1);
+        MyAlgo9 =new RealElevatorAlgo(B9);
+        ElevatorList=new Elevator[B1.numberOfElevetors()];
+        for (int i = 0; i < ElevatorList.length; i++) {
+            ElevatorList[i]=MyAlgo1.getBuilding().getElevetor(i);
+        }
+
 
     }
-}
+
+    @Test
+    void allocateAnElevator() {
+        int elev=MyAlgo1.allocateAnElevator(c.get(0));
+        assertEquals(elev,0);
+
+
+
+
+    }
+
+    @Test
+    void cmdElevator() {
+        MyAlgo1.allocateAnElevator(c.get(0));
+        MyAlgo1.cmdElevator(0);
+        for (int i = 0; i <ElevatorList.length ; i++) {
+            assertEquals(1,ElevatorList[i].getState());
+        }
+
+
+
+    }
+    }
